794c419f23d7623c8a31450f517408eb38b3791d
yf20230412 <yufeng1688778@163.com>
2023-11-16 12:57:20 +0800

    更新博客

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass,Fire,Weixin,EditRegular,CartPlus,RProject,Camera,DiceD6,BookDead,Cannabis,Fan,BalanceScaleLeft,JsSquare,FileCsv,LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass,Fire,Weixin,EditRegular,CartPlus,RProject,Camera,DiceD6,BookDead,Cannabis,Fan,BalanceScaleLeft,JsSquare,FileCsv,MapMarkedAlt,LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -82,6 +82,7 @@ const siteIcon = {
   BalanceScaleLeft,
   JsSquare,
   FileCsv,
+  MapMarkedAlt,
   LaptopCode,
 };
 
