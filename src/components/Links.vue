eb36836425d14ecc076d23e69696a01a4d8ef1a0
yf20230412 <yufeng1688778@163.com>
2023-12-03 14:35:44 +0800

    更新1203

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass,Fire,Weixin,EditRegular,CartPlus,RProject,Camera,DiceD6,BookDead,Cannabis,Fan,BalanceScaleLeft,JsSquare,FileCsv,MapMarkedAlt,LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass,Fire,Weixin,EditRegular,CartPlus,RProject,Camera,DiceD6,BookDead,Cannabis,Fan,BalanceScaleLeft,JsSquare,FileCsv,MapMarkedAlt,GooglePay,Bookmark,Tv,Book,BroadcastTower,Apple,ShareAlt,Rocket,Hammer,Markdown,Desktop,Disease,HddRegular,CloudDownloadAlt,AppleAlt,Android,Briefcase,Barcode,StarAndCrescent,Palette,LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -83,6 +83,26 @@ const siteIcon = {
   JsSquare,
   FileCsv,
   MapMarkedAlt,
+  GooglePay,
+  Bookmark,
+  Tv,
+  Book,
+  BroadcastTower,
+  Apple,
+  ShareAlt,
+  Rocket,
+  Hammer,
+  Markdown,
+  Desktop,
+  Disease,
+  HddRegular,
+  CloudDownloadAlt,
+  AppleAlt,
+  Android,
+  Briefcase,
+  Barcode,
+  StarAndCrescent,
+  Palette,
   LaptopCode,
 };
 
